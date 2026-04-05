b11a34c989c469a69dee7766134c9c30a1cfbd7c
Boolat Kamalov <800147@gmail.com>
2026-04-05 09:58:30 +0300

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+// import { __, appendChildren, used } from 'https://800147.github.io/tools/src/el/el.js';
+// import { getUrlParams } from 'https://800147.github.io/tools/src/getUrlParams/getUrlParams.js';
+// import { ModController } from 'https://800147.github.io/tools/src/ModController/ModController.js';
+
+console.log("hello");
